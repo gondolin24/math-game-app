934ee0ab7d8066638ed8dcb0236a142ab9348ca3
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2019-05-20 15:27:45 -0400

    added some random engine

--- math-game-app/src/app/home/home.page.ts
+++ math-game-app/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Equation, MathService} from "../services/math.service";
 
 @Component({
     selector: 'app-home',
@@ -6,7 +7,25 @@ import {Component} from '@angular/core';
     styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    constructor(private mathService: MathService) {
+    }
+
+    equationNumberOne: number = 0;
+    equationNumberTwo: number = 10;
+    answerOne: number = 0;
+    answerTwo: number = 10;
+    answerThree: number = 0;
+
     difficultyChange(changeEvent: any) {
-        console.log("hi")
+
+       const equation: Equation = this.mathService.getEquation(changeEvent.detail.value);
+
+        this.equationNumberOne = equation.numberOne;
+        this.equationNumberTwo = equation.numberTwo;
+    }
+
+
+    private setButtons(equation: Equation){
+
     }
 }
--- math-game-app/src/app/services/math.service.ts
+++ math-game-app/src/app/services/math.service.ts
@@ -1,10 +1,57 @@
 import {Injectable} from "@angular/core";
 
+
+export class Equation {
+    numberOne: number;
+    numberTwo: number;
+    answer: number;
+    badAnswerOne: number;
+    badAnswerTwo: number;
+}
+
 @Injectable({
     providedIn: 'root',
 })
 export class MathService {
 
+    getEquation(difficulty: string): Equation {
+
+        const max: number = this.getNumberRange(difficulty);
+        let finalEquation: Equation = new Equation();
+        finalEquation.numberOne = this.getRandomInt(max);
+        finalEquation.numberTwo = this.getRandomInt(max);
+        finalEquation.answer = finalEquation.numberTwo + finalEquation.numberOne;
+        finalEquation.badAnswerOne = this.getBadAnswer(max);
+        finalEquation.badAnswerTwo = this.getBadAnswer(max);
+        return finalEquation;
+    }
+
+
+    private getBadAnswer(max: number): number {
+        const spread = Math.floor(max * .1);
+        if (this.getRandomInt(10) > 5) {
+            return this.getRandomInt(max - spread)
+        } else {
+            return this.getRandomInt(max + spread)
+        }
+
+    }
+
+    private getNumberRange(difficulty: string): number {
+        if (difficulty === "easy") {
+            return 10;
+        } else if (difficulty === "medium") {
+            return 100;
+        } else if (difficulty === "hard") {
+            return 1000;
+        } else if (difficulty === "master") {
+            return 10000;
+        }
+    }
+
+    private getRandomInt(max): number {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
 
 }
 
